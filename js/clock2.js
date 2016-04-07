6b57afb0771baf8b7b2fb28836826a36d346549e
Dave Harned <dharned@gmail.com>
2016-04-07 17:54:07 -0500

    start function

--- js/clock2.js
+++ js/clock2.js
@@ -1,24 +1,42 @@
 // REFACTOR clock.js INTO JQUERY
-$(document).ready(function() {
+$(document).ready(function () {
     var running = false,
         newPomMin,
-        newBrkMin;
+        newBrkMin,
+        workTimeMin,
+        workTimeSec;
 
     // CLOCK CONTROL FUNCTIONS
-//    $('#start').on('click', startTimer() {
-//                   
-//    });
-//
-//    $('#pause').on('click', pauseTimer() {
-//                   
-//    });  
-//
-//    $('#reset').on('click', resetTimer() {
-//                   
-//    });
+    $('#start').on('click', startTimer() {
+        console.log("start timer");
+        workTimeMin = parseInt($('#pomodoroMin').html, 10);
+        workTimeSec = workTimeMin * 60;
+        countdownId = setInterval("counter()", 1000);
+        running = true;
+
+        // check second hand state, start or resume animation
+        if ( hand.className.match(/(?:^|\s)hand-stopped(?!\S)/) ) {
+        hand.className = hand.className.replace( /(?:^|\s)hand-stopped(?!\S)/g , '' );  
+        } else if ( hand.className.match(/(?:^|\s)hand-paused(?!\S)/) ) {
+        hand.className = hand.className.replace( /(?:^|\s)hand-paused(?!\S)/g , '' );  
+        }
+
+        // Change buttons displayed
+        start.style.display = "none";
+        pause.style.display = "inline";
+        reset.style.display = "none";
+    });
+
+    $('#pause').on('click', pauseTimer() {
+                   
+    });  
+
+    $('#reset').on('click', resetTimer() {
+                   
+    });
+    
     
     // TIME SELECTOR FUNCTIONS
-    
     $('#pomUp').on('click', function() {
         newPomMin = parseInt($('#pomodoroMin').html(), 10);
             if (running === false && newPomMin < 99){
